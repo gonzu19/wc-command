1cbc2d154a4b3be9e4341e1f1e587fd005f1b4ea
Gonzalo <gmartipo@emeal.nttdata.com>
2024-06-26 08:23:01 +0200

    Changed the test to conform to the new functionality

--- test_main.py
+++ test_main.py
@@ -5,12 +5,15 @@ from main import (checklines, checkwords, checkcharacters,
 
 filename = 'text.txt'
 
+with open(filename, "r") as file:
+    file_content = file.readlines()
+
 
 def test_line_count() -> None:
     result = subprocess.run(['wc', '-l', filename], stdout=subprocess.PIPE)
     output = result.stdout.decode('utf-8').split()
     n_original_wc = int(output[0])
-    n_custom_wc = checklines(filename)
+    n_custom_wc = checklines(file_content)
     assert n_custom_wc == n_original_wc
 
 
@@ -18,7 +21,7 @@ def test_words_count() -> None:
     result = subprocess.run(['wc', '-w', filename], stdout=subprocess.PIPE)
     output = result.stdout.decode('utf-8').split()
     n_original_wc = int(output[0])
-    n_custom_wc = checkwords(filename)
+    n_custom_wc = checkwords(file_content)
     assert n_custom_wc == n_original_wc
 
 
@@ -26,5 +29,5 @@ def test_characters_count() -> None:
     result = subprocess.run(['wc', '-m', filename], stdout=subprocess.PIPE)
     output = result.stdout.decode('utf-8').split()
     n_original_wc = int(output[0])
-    n_custom_wc = checkcharacters(filename)
+    n_custom_wc = checkcharacters(file_content)
     assert n_custom_wc == n_original_wc
